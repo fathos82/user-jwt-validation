ef1b5de0813c643cb64676493ee0ecfa14f5aa4e
Athos Ferreira <fathos82@gmail.com>
2024-07-24 16:03:58 -0300

    Adding last configurations!

---
+++ src/test/java/com/example/userjwtvalidation/UserJwtValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.userjwtvalidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserJwtValidationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
